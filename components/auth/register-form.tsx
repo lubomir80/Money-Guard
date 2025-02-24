fc446300b8d7eef463db625c7660fa9b466612ab
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-02-24 19:54:02 +0100

    Register form

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,8 +1,141 @@
-import React from 'react'
+"use client"
+
+import CardWrapper from '../card/CardWrapper'
+import { FaUser } from "react-icons/fa";
+import { IoMdLock } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { RegisterSchema, TRegisterSchema } from "@/schemas/index"
+import {
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import FormButtons from '../FormButtons';
 
 function RegisterForm() {
+   const form = useForm<TRegisterSchema>({
+      resolver: zodResolver(RegisterSchema),
+      defaultValues: {
+         name: "",
+         email: "",
+         password: "",
+         confirmPassword: ""
+      }
+   })
+
+   const onSubmit = (values: TRegisterSchema) => {
+      console.log({ values });
+      form.reset()
+   }
+
+
    return (
-      <div>RegisterForm</div>
+      <CardWrapper headerLogo>
+         <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}
+               className="space-y-10 text-left">
+               <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                     <FormItem className="relative">
+                        <FaUser className="absolute top-2 w-4 h-4 text-whiteText/30" />
+                        <FormControl>
+                           <Input
+                              className="
+                            pl-6
+                            border-b-2 
+                            border-whiteText/30
+                            placeholder:text-whiteText/30
+                            text-whiteText
+                            focus:border-whiteText"
+                              placeholder="Name"
+                              {...field}
+                              type="text" />
+                        </FormControl>
+                        <FormMessage />
+                     </FormItem>
+                  )}
+               />
+               <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                     <FormItem className="relative">
+                        <MdEmail className="
+                           absolute top-2 w-4 h-4 text-whiteText/30" />
+                        <FormControl >
+                           <Input
+                              className="
+                                 pl-6
+                                 border-b-2 
+                                 border-whiteText/30
+                                 placeholder:text-whiteText/30
+                                 text-whiteText
+                                 focus:border-whiteText"
+                              placeholder="E-mail"
+                              {...field}
+                              type="email" />
+                        </FormControl>
+                        <FormMessage />
+                     </FormItem>
+                  )}
+               />
+               <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                     <FormItem className="relative">
+                        <IoMdLock className="absolute top-2 w-4 h-4 text-whiteText/30" />
+                        <FormControl>
+                           <Input
+                              className="pl-6 border-b-2 border-whiteText/30
+                              placeholder:text-whiteText/30 text-whiteText
+                              focus:border-whiteText"
+                              placeholder="Password"
+                              {...field}
+                              type="password" />
+                        </FormControl>
+                        <FormMessage />
+                     </FormItem>
+                  )}
+               />
+               <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                     <FormItem className="relative">
+                        <IoMdLock className="absolute top-2 w-4 h-4 text-whiteText/30" />
+                        <FormControl>
+                           <Input
+                              className="
+                            pl-6
+                            border-b-2 
+                            border-whiteText/30
+                            placeholder:text-whiteText/30
+                            text-whiteText
+                            focus:border-whiteText"
+                              placeholder="Confirm password"
+                              {...field}
+                              type="password" />
+                        </FormControl>
+                        <FormMessage />
+                     </FormItem>
+                  )}
+               />
+               <FormButtons
+                  actionButtonLabel='Register'
+                  backButtonHref='/auth/login'
+                  backButtonLabel=' Log in'
+               />
+            </form>
+         </Form>
+      </CardWrapper>
    )
 }
 
--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,27 @@ export const LoginSchema = z.object({
    })
 })
 export type TLoginSchema = z.infer<typeof LoginSchema>
+
+export const RegisterSchema = z.object({
+   name: z.string().min(3, {
+      message: "Minimum 3 characters required"
+   }),
+   email: z.string().email({
+      message: "Email is required"
+   }),
+   password: z.string().min(6, {
+      message: "Minimum 6 characters required"
+   }),
+   confirmPassword: z.string().min(6, {
+      message: 'Please confirm your password'
+   }),
+}).superRefine((val, ctx) => {
+   if (val.password !== val.confirmPassword) {
+      ctx.addIssue({
+         code: z.ZodIssueCode.custom,
+         message: 'Password is not the same as confirm password',
+         path: ['confirmPassword'],
+      })
+   }
+})
+export type TRegisterSchema = z.infer<typeof RegisterSchema>
