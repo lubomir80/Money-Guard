df9b5a76f70b281f5207b923799995e032bc4a64
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-03-19 16:09:29 +0100

    Logo fix 2

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import LogoImg from "@/public/logo.png"
+
+
 
 
 function Logo({ small }: { small?: boolean }) {
    if (small) {
       return (
          <div className="flex items-center justify-center flex-col">
-            <Image src="/logo.png" alt="" width={24} height={24} />
+            <Image src={LogoImg} alt="" width={24} height={24} />
             <h1 className={"text-[18px] drop-shadow-md"}>
                Money Guard
             </h1>
@@ -15,7 +18,7 @@ function Logo({ small }: { small?: boolean }) {
 
    return (
       <div className="flex items-center justify-center flex-col">
-         <Image src="/logo.png" alt="" width={36} height={36} />
+         <Image src={LogoImg} alt="" width={36} height={36} />
          <h1 className={"text-[27px] drop-shadow-md"}>
             Money Guard
          </h1>
