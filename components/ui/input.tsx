c4247550c21b021fe8472dcfc614b3e76f0cb31a
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-04-29 17:46:35 +0200

    Input transparent styled

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
          <input
             type={type}
             className={cn(
-               "flex h-9 w-full bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm   autofill:shadow-[inset_0_0_0px_1000px_rgb(122,86,161)] ",
+               "flex h-9 w-full bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
                className
             )}
             ref={ref}
