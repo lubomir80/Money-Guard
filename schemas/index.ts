50971f22965b879a73e1279dee6270f5e73c16b3
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-05-05 14:32:39 +0200

    comment limited

--- schemas/index.ts
+++ schemas/index.ts
@@ -57,9 +57,11 @@ export type TNewPasswordSchema = z.infer<typeof NewPasswordSchema>
 export const AddTransactionSchema = z.object({
    type: z.boolean(),
    category: z.string(),
-   comment: z.optional(z.string()
-      .min(5, { message: "Min 5 characters" })
-      .max(40, { message: "Max 40 characters" })),
+   comment: z.optional(z.string().min(5, {
+      message: "Min 5 characters"
+   }).max(40, {
+      message: "Max 40 characters"
+   })),
    amount: z.coerce.number().positive({
       message: "Must be a positive"
    }),
@@ -86,9 +88,9 @@ export type TAddTransactionSchema = z.infer<typeof AddTransactionSchema>
 export const EditTransactionSchema = z.object({
    type: z.boolean(),
    category: z.string(),
-   comment: z.optional(z.string()
-      .min(5, { message: "Min 5 characters" })
-      .max(40, { message: "Max 40 characters" })),
+   comment: z.optional(z.string().min(5), {
+      message: "Min 5 characters"
+   }),
    amount: z.coerce.number().positive({
       message: "Must be a positive"
    }),
